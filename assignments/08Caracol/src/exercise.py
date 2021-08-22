f7f9ae6760bc116992ebb397a22a381c26bf8ced
KarenP1525 <89099490+KarenP1525@users.noreply.github.com>
2021-08-21 19:52:14 -0500

    Update exercise.py
    -

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
+    minutos=int(input("Dame los minutos: "))
+    conver1=5.7/10
+    recorrido=conver1*60
+
+    print("Centímetros recorridos: ",recorrido)
+
+
+
     pass
 
 if __name__ == '__main__':
