ddffc4b5d13c0554497ea875f971d04d173e8d53
KarenP1525 <89099490+KarenP1525@users.noreply.github.com>
2021-08-21 20:07:48 -0500

    Update exercise.py
    juegos

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-   pass
+    nuevos=int(input("Dame la cantidad de juegos nuevos: "))
+    usados=int(input("Dame la cantidad de juegos usados: "))
+
+    tot=(nuevos*1000)+(usados*350)
+    print("El total de la es: ",tot)
+    pass
 
 
 
