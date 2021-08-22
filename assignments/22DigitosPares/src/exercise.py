27bfe2937e16adb9d6734c2e0cd1ea6e4a1cf3e5
KarenP1525 <89099490+KarenP1525@users.noreply.github.com>
2021-08-22 14:17:55 -0500

    Update exercise.py
    Para números de 4 dígitos de da cuantos son pares

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,5 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+    numero=(input("Dame un número"))
+
+    
+    x=0
+    count=0
+    lista=[int(i)for i in numero]
+    if len(lista)==4 and lista.count(0)==0:
+        while x<len(lista):
+            if (math.fmod(lista[x],2))==0:
+                    count=count+1
+            x=x+1    
+
+        print("El número de pares es: ",count)
+    else:
+        print("lo siento, ingresa un número de 4 dígitos y que no empiece con 0")
+
+
+
     pass
 
 
